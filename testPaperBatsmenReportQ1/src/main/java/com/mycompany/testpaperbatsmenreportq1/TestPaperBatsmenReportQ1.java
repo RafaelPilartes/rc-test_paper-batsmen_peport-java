23264ee98089bc1600a510168d3347ffd5d5cb31
Rafael Pilartes <167252457+CyberWolfKingg@users.noreply.github.com>
2025-09-29 14:54:09 +0200

    feat - Q1: Loop input

--- testPaperBatsmenReportQ1/src/main/java/com/mycompany/testpaperbatsmenreportq1/TestPaperBatsmenReportQ1.java
+++ testPaperBatsmenReportQ1/src/main/java/com/mycompany/testpaperbatsmenreportq1/TestPaperBatsmenReportQ1.java
@@ -12,5 +12,20 @@ public class TestPaperBatsmenReportQ1 {
         // dimentional array
         int[][] runsScored = new int[3][3];
         
+        System.out.println("SA CRICKETER APPLICATION");
+        System.out.println("------------------------------------------------------------");
+        
+        // Loop input
+        for (int i = 0; i < stadiums.length; i++) {
+            System.out.print("Enter the number runs scored by " + batsmen[0] + " at " + stadiums[i] + ": ");
+            runsScored[0][i] = sc.nextInt();
+            
+            System.out.print("Enter the number runs scored by " + batsmen[1] + " at " + stadiums[i] + ": ");
+            runsScored[1][i] = sc.nextInt();
+            
+            System.out.print("Enter the number runs scored by " + batsmen[2] + " at " + stadiums[i] + ": ");
+            runsScored[2][i] = sc.nextInt();
+        }
+
         
 }
